e5cb911fed472b794d0903537d33c72bacdbc3f4
Rich Williams <foodwebster@gmail.com>
2017-11-08 10:13:42 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tag2Network
+Build similarity network from any dataset where a set of keywords or other tags is assigned to each document
+The network is build by computing cosine simiarity between each document, based on their tag sets, and thresholding the similarities to produce a network
+Also includes code, in WOSTags folder, to process Web of Science search results to extract keywords and prepare data to build a network
+Example code demonstrates building a document network from Web of Science data
---
+++ RunWOSExample.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# build similarity network from Web of Science data
+
+import os
+
+from WOSTags.MergeWOSFiles import mergeWOSFiles
+from WOSTags.ProcessReferences import processRawWoSData
+from WOSTags.ProcessReferences import filterDocuments
+from WOSTags.BuildKeywords import buildKeywords
+from Network.BuildNetwork import buildDocumentNetwork
+
+dropCols = ['text', 'AuKeywords', 'KeywordsPlus', 'keywords']
+
+namebase = "Example"         # dataset/project name
+rawfilebase = "savedrecs"   # name base of raw, unjoined WoS data files
+outfile = namebase+"Raw.txt"
+basepath = "Data"
+datapath = os.path.join(basepath, namebase)
+
+# concatentate multiple WOS data files
+mergeWOSFiles(datapath, rawfilebase, outfile)
+# extract smaller set of useful data from raw WOS output
+processRawWoSData(datapath, namebase)
+
+# if desired, make keyword blacklist and whitelist
+blacklist = set([])
+whitelist = set([])
+
+fname = os.path.join(datapath, namebase+"Final.txt")
+
+# too many documents - get rid of some of them
+df = filterDocuments(fname, 0.9)
+
+# set up output file names
+nwname = os.path.join(datapath, namebase+".xlsx")
+nodesname = os.path.join(datapath, namebase+"Nodes.csv")
+edgesname = os.path.join(datapath, namebase+"Edges.csv")
+
+# build the keywords, add to df
+kwAttr = buildKeywords(df, blacklist, whitelist)
+# build network linked by keyword similarity
+buildDocumentNetwork(df, kwAttr, dropCols=dropCols, outname=nwname, nodesname=nodesname, edgesname=edgesname, doLayout=True)
