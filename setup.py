70d860b90792dc62d32a00a7a5aaa81ca15186a1
Rich Williams <foodwebster@gmail.com>
2017-11-20 15:51:24 -0800

    packaging fixes

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
+# python setup.py sdist
+# python setup.py bdist_wheel --universal
+# twine upload dist/*
+
 from setuptools import setup
+from setuptools import find_packages
 
 setup(name='tag2network',
-      version='0.1',
+      version='0.0.3',
       description='Build similarity network from tagged documents',
-      url='http://github.com/foodwebster/tag2network',
-      author='Rich Williams',
+      url='https://github.com/foodwebster/tag2network',
+      author='foodwebster',
+      author_email='foodwebster@gmail.com',
       license='MIT',
-      packages=['tag2network'],
+      packages=find_packages(),
+      install_requires=[
+          'numpy', 'scipy', 'pandas', 'networkx'
+      ],
       zip_safe=False)
