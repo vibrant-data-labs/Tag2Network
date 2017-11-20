bfa2332cdeea8e730002032e5bb4b4dcfa73d372
Rich Williams <foodwebster@gmail.com>
2017-11-20 13:48:34 -0800

    refactor to make packaging easier

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tag2network',
+      version='0.1',
+      description='Build similarity network from tagged documents',
+      url='http://github.com/foodwebster/tag2network',
+      author='Rich Williams',
+      license='MIT',
+      packages=['tag2network'],
+      zip_safe=False)
