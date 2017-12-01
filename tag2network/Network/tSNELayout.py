d1d4aeb697b775e5f981475bd835ca30a8282cb7
Rich Williams <foodwebster@gmail.com>
2017-11-30 21:26:16 -0800

    improve tSNE layout performance

--- tag2network/Network/tSNELayout.py
+++ tag2network/Network/tSNELayout.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import networkx as nx
+import scipy.sparse as sps
 from sklearn.manifold import TSNE
 
 def runTSNELayout(nw, maxdist=5):
@@ -10,17 +11,16 @@ def runTSNELayout(nw, maxdist=5):
     # fill all longer paths with twice the max
     def computeShortPathUpToMax(adj, maxlen):
         dist = np.empty(adj.shape)
-        dist.fill(-1)
+        dist.fill(-1)   # initialize to -1 - any element with -1 hasn't been filled yet
         np.fill_diagonal(dist, 0)
-        cc = adj
-        for idx in range(1, maxlen):
-            dist[(dist == -1) & (cc > 0).toarray()] = idx
+        cc = sps.identity(dist.shape[0])
+        for idx in range(1, maxlen+1):
             cc = cc.dot(adj)
+            dist[(dist == -1) & (cc > 0).toarray()] = idx   # fill as yet unfilled path lengths
         dist[dist==-1] = 2.0*maxlen
         return dist
 
     print("Running tSNE layout")
-
     # compute network shortest path lengths up to a maximum threshold - beyond that things are far away
     print("Computing shortest paths")
     dists = computeShortPathUpToMax(nx.adjacency_matrix(nw), maxdist)
